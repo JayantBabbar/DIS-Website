a37efe4c7df12f7e39b1d07d97c137ab1066457d
jayantbabbar <jayantbabbar1@gmail.com>
2024-04-08 10:24:57 +0530

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+gsap.from('#logo',{
+   y:100,
+   opacity:0,
+   ease: "power4.inOut",
+   delay:1,
+   stagger:{
+    amount:0.3
+   }
+})
+
+gsap.from('.main_head_tag',{
+   x:100,
+   opacity:0,
+   ease: "power4.inOut",
+   delay:1,
+   stagger:{
+    amount:0.3
+   }
+})
